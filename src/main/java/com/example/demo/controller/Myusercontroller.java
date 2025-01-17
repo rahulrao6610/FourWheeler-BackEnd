8096448001a29c5a7bba15c35e432e178b460d71
rahulrao6610 <22bk1a6610@stpetershyd.com>
2025-01-17 17:14:57 +0530

    second commit

--- src/main/java/com/example/demo/controller/Myusercontroller.java
+++ src/main/java/com/example/demo/controller/Myusercontroller.java
@@ -1,19 +1,28 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.service.Myuserservice;
+
 @RestController
-@CrossOrigin
+@CrossOrigin("*")
 public class Myusercontroller {
-     @GetMapping("four-wheeler/user/isValidUser")
+	
+	@Autowired
+	Myuserservice myuserser;
+	
+    @GetMapping("four-wheeler/user/isValidUser")
 	public boolean checkUser(@RequestParam String userName,String password) {
     	 System.out.println("from React Data---->");
     	 System.out.println("from React Data"+userName);
     	 System.out.println("from React Data"+password);
-		return false;
+    	 
+    	 Boolean res=myuserser.checkUserIsValid(userName,password);
+		return res;
 		
 	}
 	
--- src/main/java/com/example/demo/service/Myuserservice.java
+++ src/main/java/com/example/demo/service/Myuserservice.java
@@ -1,9 +1,27 @@
 package com.example.demo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Myuser;
+import com.example.demo.repo.Myuserrepo;
 
 @Service
 public class Myuserservice {
 
-	
+	@Autowired
+	Myuserrepo myuserrepo;
+	public Boolean checkUserIsValid(String userName, String password) {
+		
+		Boolean res=false;
+		Myuser myuser=myuserrepo.findById(userName).orElse(new Myuser());
+		if(myuser.getUserName()!=null) {
+		   if(myuser.getUserName().contentEquals(userName)) {
+			 if(myuser.getPassword().contentEquals(password)) {
+	            res = true;
+		}
+	}
+}
+	return res;
+	}
 }
